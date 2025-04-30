3bc4d46651056d28720a746c2efb3bbe455b7979
khushi123-kumari <123khush456@gmail.com>
2025-04-30 21:26:33 +0530

    Add files via upload

---
+++ java/main3.java
@@ -0,0 +1,9 @@
+public class main3 {
+    public static void main(String[] args){
+        int a=10;
+        int b=25;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    
+}
---
+++ java/main4.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a = sc.next Int();
+        int b = sc.next Int();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    
+}
